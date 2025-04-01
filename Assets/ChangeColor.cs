094359045cc2a26c0a87a4208d9709264013d083
Simon Onyszczak Hansen <simonhansen0811@gmail.com>
2025-04-01 07:49:43 +0200

    added a sphere, and edited the script so it cycles between the different chakra colors

--- Assets/ChangeColor.cs
+++ Assets/ChangeColor.cs
@@ -1,25 +1,44 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ChangeColor : MonoBehaviour
 {
-    public Color newBaseColor = Color.red;
+    [SerializeField]
+    private Material myMaterial;
 
-    private Renderer cubeRenderer;
-    // Start is called before the first frame update
+    private Color[] chakraColor = new Color[]
+    {
+        new Color(0.8f, 0f, 0f), // Root Chakra – Deep Red
+        new Color(1f, 0.2f, 0f), // Sacral Chakra – Vivid Orange
+        new Color(1f, 0.85f, 0f), // Solar Plexus Chakra – Golden Yellow
+        new Color(0f, 0.8f, 0f), // Heart Chakra – Rich Green
+        new Color(0f, 0.8f, 0.8f), // Throat Chakra – Pure Turquoise
+        new Color(0f, 0f, 1f), // Third Eye Chakra – Deep Indigo
+        new Color(0.2f, 0f, 0.4f) // Crown Chakra – Soft Violet
+
+    };
+
+    private int currentColorIndex = 0;
+    private float colorChangeInterval = 2f;
     void Start()
     {
-        cubeRenderer = GetComponent<Renderer>();
-        cubeRenderer.material.color = newBaseColor;
-
-        cubeRenderer.material.EnableKeyword("_EMISSION");
-        cubeRenderer.material.SetColor("_EmissionColor", newBaseColor);
+        StartCoroutine(ChakraColorChange());
     }
 
-    // Update is called once per frame
+    IEnumerator ChakraColorChange()
+    {
+        while (true)
+        {
+            myMaterial.color = chakraColor[currentColorIndex];
+            myMaterial.EnableKeyword("_EMISSION");
+            myMaterial.SetColor("_EmissionColor", chakraColor[currentColorIndex]);
+            currentColorIndex = (currentColorIndex + 1) % chakraColor.Length;
+            yield return new WaitForSeconds(colorChangeInterval);
+        }
+    }
+
     void Update()
     {
-        
+
     }
 }
