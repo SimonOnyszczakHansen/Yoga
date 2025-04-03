51c507e02354cf5111ec75d6a5ad57f99aa6d061
andr515i <andr515i@zbc.dk>
2025-04-03 09:10:26 +0200

    color changes

--- Assets/ChangeColor.cs
+++ Assets/ChangeColor.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class ChangeColor : MonoBehaviour
 {
-    [SerializeField] private Material myMaterial;
-    [SerializeField] private AudioSource myAudioSource;
-    [SerializeField] private AudioClip[] myAudioClips;
+    [SerializeField]
+    private Material myMaterial;
+
+    [SerializeField]
+    private AudioSource myAudioSource;
+
+    [SerializeField]
+    private AudioClip[] myAudioClips;
 
     private Color[] chakraColors = new Color[]
     {
@@ -14,14 +19,14 @@ public class ChangeColor : MonoBehaviour
         new Color(1f, 0.2f, 0f), // Sacral Chakra – Orange
         new Color(1f, 0.85f, 0f), // Solar Plexus Chakra – Yellow
         new Color(0f, 0.8f, 0f), // Heart Chakra – Green
-        new Color(0f, 0.8f, 0.8f), // Throat Chakra – Light Blue
-        new Color(0f, 0f, 1f), // Third Eye Chakra – Indigo
-        new Color(0.2f, 0f, 0.4f) // Crown Chakra – Violet
-
+        new Color(0.5f, 0.8f, 0.9f), // Throat Chakra – Light-Blue
+        new Color(0.4f, 0f, 0.925f), // Third Eye Chakra – Indigo
+        new Color(0.3f, 0f, 0.5f), // Crown Chakra – Purple with a hint of White
     };
 
     private int currentColorIndex = 0;
     private int currentAudioIndex = 0;
+
     void Start()
     {
         StartCoroutine(ChakraColorChange());
@@ -32,6 +37,7 @@ public class ChangeColor : MonoBehaviour
         myAudioSource.clip = myAudioClips[index];
         myAudioSource.Play();
     }
+
     private IEnumerator ChakraColorChange()
     {
         while (true)
@@ -44,7 +50,9 @@ public class ChangeColor : MonoBehaviour
 
             PlayAudio(currentAudioIndex);
 
-            Debug.Log("Current Clip: " + currentAudioIndex  + " Current Color: " + currentColorIndex);
+            Debug.Log(
+                "Current Clip: " + currentAudioIndex + " Current Color: " + currentColorIndex
+            );
 
             yield return new WaitForSeconds(myAudioClips[currentAudioIndex].length);
 
@@ -53,8 +61,5 @@ public class ChangeColor : MonoBehaviour
         }
     }
 
-    void Update()
-    {
-
-    }
+    void Update() { }
 }
